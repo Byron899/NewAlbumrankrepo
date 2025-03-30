bcd73d73319e72bf84064a699a7c8922d3139352
Byron899 <byronhughesr34@icloud.com>
2025-03-30 20:04:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 
-const API_URL = "https://albumbackende-1.onrender.com";
+const API_URL = "https://albumbackende-1.onrender.com/albums";
 
 function App() {
   const [albums, setAlbums] = useState([]);
